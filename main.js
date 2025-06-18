8d5906336465bd263d5cb7805306ff7d5127739e
19seniman <161638164+19seniman@users.noreply.github.com>
2025-06-18 20:38:38 +0700

    main.js

--- main.js
+++ main.js
@@ -25,13 +25,13 @@ const logger = {
   success: (msg) => console.log(`${colors.green}[✅] ${msg}${colors.reset}`),
   loading: (msg) => console.log(`${colors.cyan}[⟳] ${msg}${colors.reset}`),
   step: (msg) => console.log(`${colors.white}[➤] ${msg}${colors.reset}`),
-  debug: (msg) => console.log(`${colors.gray}[…] ${msg}${colors.reset}`), 
-  critical: (msg) => console.log(`${colors.red}${colors.bold}[❌] ${msg}${colors.reset}`), 
+  countdown: (msg) => process.stdout.write(`\r${colors.blue}[⏰] ${msg}${colors.reset}`),
   banner: () => {
-    console.log(`${colors.cyan}${colors.bold}`);
-    console.log(`---------------------------------------------`);
-    console.log(` 19Seniman From Insiders - PLEASE SUPPORT PALESTINE 🍉🍉🍉 `);
-    console.log(`---------------------------------------------${colors.reset}`);
+    const { cyan, magenta, reset } = colors;
+    console.log(magenta + '=============================================' + reset);
+    console.log(cyan + '  🍉🍉PLEASE SUPPORT PALESTINE ON SOCIAL MEDIA 🍉🍉 ' + reset);
+    console.log(cyan + '       19Senniman from Insider' + reset);
+    console.log(magenta + '=============================================' + reset);
     console.log();
   }
 };
